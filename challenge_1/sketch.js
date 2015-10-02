3be00b6a281e2470040d02a5498b4080ec2f7817
nicosiy <nicosiysoo@gmail.com>
2015-10-02 00:54:30 -0400

    Challenge one commit

--- challenge_1/sketch.js
+++ challenge_1/sketch.js
@@ -1,18 +1,45 @@
+//Variable statements
+var xPos = [];
+var yPos = [];
+
 function setup() {
-	// create a place to draw
-	createCanvas(640, 360);
+	// Create Canvas
+	createCanvas(640, 480);
 	noStroke();
 	noLoop();
+	colorMode(RGB);
+	background(100, 185, 235);
+	fill(57, 177, 74);
+	rect(0, 280, 640, 200);
+	for (var i = 0; i < 40; i++) {
+		xPos[i] = random(0, 640);
+		yPos[i] = random(200, 400);
+	}
 }
 
 function draw() {
-	// clear the background
-	background(50, 50, 150);
+	var size = 40;
+	for (var i = 0; i < 40; i++) {
+		// Stems
+		fill(38, 120, 55);
+		rect(xPos[i] - 2.5, yPos[i] - 2.5, 5, 100, 20);
+		ellipse(xPos[i], yPos[i], 15, 15);
+		//Flowers:
+		// 1st Sphere
+		fill(255, 255, 255, 85);
+		ellipse(xPos[i], yPos[i], size, size);
 
-	// set a fill color
-	fill(255, 255, 255);
+		// 2nd Sphere
+		fill(255, 255, 255, 90);
+		ellipse(xPos[i], yPos[i], size - 10, size - 10);
 
-	// draw the ellipse
-	var diameter = random(100, 200);
-	ellipse(320, 180, diameter, diameter);
+		// 3rd Sphere
+		fill(255, 255, 255, 95);
+		ellipse(xPos[i], yPos[i], size - 20, size - 20);
+
+		// 4th Sphere
+		fill(255, 255, 255, 100);
+		ellipse(xPos[i], yPos[i], size - 30, size - 30);
+
+	}
 }
