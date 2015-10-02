8de958430368989948a3335cb39e5fa53a3d3486
nicosiy <nicosiysoo@gmail.com>
2015-10-02 01:14:40 -0400

    challenge four commit

--- challenge_4/sketch.js
+++ challenge_4/sketch.js
@@ -1,18 +1,31 @@
+//Stating Variables
+var xPosition = [];
+var yPosition = [];
+
+//Create Canvas
 function setup() {
-	// create a place to draw
-	createCanvas(640, 360);
+	createCanvas(650, 500);
 	noStroke();
-	noLoop();
+	for (var i = 0; i < 10; i++) {
+		xPosition[i] = random(0, 650);
+		yPosition[i] = random(0, 500);
+
+	}
 }
 
+//Draw Bg and Balls
 function draw() {
-	// clear the background
-	background(150, 50, 50);
+	background(150, 0, 0);
 
-	// set a fill color
-	fill(255, 255, 255);
+	for (var i = 0; i < 10; i++) {
+		xPosition[i] = xPosition[i] + random(-4, 4);
+		yPosition[i] = yPosition[i] + random(-4, 4);
+		fill(0, 255, 255);
+		ellipse(xPosition[i] + 240, yPosition[i] - 130, 5, 5);
 
-	// draw the ellipse
-	var diameter = random(100, 200);
-	ellipse(320, 180, diameter, diameter);
+		ellipse(xPosition[i] - 90, yPosition[i] + 180,
+			12, 12);
+
+		ellipse(xPosition[i], yPosition[i], 25, 25);
+	}
 }
