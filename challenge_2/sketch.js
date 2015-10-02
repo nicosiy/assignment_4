54c17d0e9d866fb70af05309a3d3f4dd44be8934
nicosiy <nicosiysoo@gmail.com>
2015-10-02 01:01:50 -0400

    challenge two commit

--- challenge_2/sketch.js
+++ challenge_2/sketch.js
@@ -1,95 +1,76 @@
-// challenge_2.js
+	//Variable statements
 
-// In this challenge a ball bounces around the screen.
-// The ball bounces when it hits the side of the canvas
-// and when it hits a mouse controlled paddle.
+	// Stating Paddle Variable
+	var paddle = {
+		x: 0,
+		y: 350,
+		width: 300,
+		height: 30,
+	};
 
-// This challenge contains multiple syntax, runtime, and 
-// logical errors. Find and fix the errors to make it work.
-
-
-// ball: a data object to hold info about the ball
-var ball = {
-	x: 500,
-	y: 100,
-	speedX: 5,
-	speedY: 5,
-	radius: 25
-};
-
-
-// paddle: a data object to hold info about the ball
-var paddle = {
-	x: 0,
-	y: 350
-	width: 300,
-	height: 30,
-};
+	// Stating Ball Variable
+	var ball = {
+		x: 500,
+		y: 100,
+		speedX: 5,
+		speedY: 5,
+		radius: 25
+	};
 
 
-// setup: called by p5 once at startup
-setup() {
-	createCanvas(640, 480);
-	noStroke();
-}
-
-
-// draw: called by p5 repeatedly to draw the game
-function draw() {
-	
-	//////////////////////////////
-	// update
-
-	updateBall); 
-	paddle.x = mouseY;
-
-
-	//////////////////////////////
-	// draw
-
-	background(50, 50, 50);
-	circle(ball.x, ball.y, ball.radius * 2, ball.radius * 2);
-	square(paddle.x - paddle.width * 0.5, paddle.y, paddle.width, paddle.height);
-
-}
-
-
-// updateBall: move the ball, then detect and respond to collisions
-function updateBall() 
-{
-	// change position
-	ball.x = ball.x + ball.speedX;
-	ball.y = ball.y + ball.speedY;
-	
-
-	// don't let the ball go through the walls
-
-	// right
-	if (ball.x > width - ball.radius) {
-		ball.speedX = -ball.speedX;
+	//Ceate Canvas
+	function setup() {
+		createCanvas(640, 480);
+		noStroke();
 	}
 
-	// bottom
-	if (ball.y > height - ball.radius) {
-		ball.speedY = ball.speedY;
+	function draw() {
+
+		refreshBall();
+		paddle.x = mouseX;
+
+		background(50, 50, 50);
+		ellipse(ball.x, ball.y, ball.radius * 2, ball.radius * 2);
+		rect(mouseX, paddle.y, paddle.width, paddle.height);
+
 	}
 
-	// left
-	if (ball.x < 0 + ball.radius) {
-		ball.speedX = -ball.speedX;
-	}
 
-	// top
-	if (ball.y < 0 + ball.radius) {
-		ball.speedY = -ball.speedY;
-	}
+	// refreshBall Function. Moves ball and detects collisions
+	function refreshBall() {
+		// change position
+		ball.x = ball.x + ball.speedX;
+		ball.y = ball.y + ball.speedY;
 
-	// check for a *collision* between the ball and the paddle
-	// look up the && operator
 
-	if (ball.x > (paddle.x - paddle.width * 0.5) && ball.x < (paddle.x + paddle.width * 0.5) && ball.y > paddle.y - ball.radius) {
-		if (ball.speedY > 0) {
+		// Walls
+		// Right
+		if (ball.x > 640 - ball.radius) {
+			ball.speedX = -ball.speedX;
+		}
+
+		// Bottom
+		if (ball.y > 480 - ball.radius) {
 			ball.speedY = -ball.speedY;
 		}
+
+		// Left
+		if (ball.x < 0 + ball.radius) {
+			ball.speedX = -ball.speedX;
+		}
+
+		// Top
+		if (ball.y < 0 + ball.radius) {
+			ball.speedY = -ball.speedY;
+		}
+
+		// check for a *collision* between the ball and the paddle
+		// look up the && operator
+
+		if (ball.x > (paddle.x - paddle.width + 300) && ball.x < (paddle.x + paddle.width) &&
+			ball.y > (paddle.y - ball.radius)) {
+			if (ball.speedY > 0) {
+				ball.speedY = -ball.speedY;
+			}
+		}
 	}
-}
